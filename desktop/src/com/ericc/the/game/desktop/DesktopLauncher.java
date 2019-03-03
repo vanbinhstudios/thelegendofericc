2af0921fc9ca3c94585e29086d46278cc5f10125
fiffeek <fm394471@students.mimuw.edu.pl>
2019-03-03 18:02:44 +0100

    Make project wireframe

---
+++ desktop/src/com/ericc/the/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ericc.the.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ericc.the.game.TheLegendOfEricc;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TheLegendOfEricc(), config);
+	}
+}
