6def1fce93934468147c377bca384ce64f0b2351
Andrzej Głuszak <gluszak.andrzej@gmail.com>
2019-03-24 16:11:06 +0100

    minor fixes

--- core/src/com/ericc/the/game/systems/logic/TeleportPlayerSystem.java
+++ core/src/com/ericc/the/game/systems/logic/TeleportPlayerSystem.java
@@ -1,7 +1,6 @@
 package com.ericc.the.game.systems.logic;
 
 import com.badlogic.ashley.core.EntitySystem;
-import com.ericc.the.game.Engines;
 import com.ericc.the.game.Mappers;
 import com.ericc.the.game.actions.Actions;
 import com.ericc.the.game.actions.TeleportAction;
@@ -14,14 +13,11 @@ import com.ericc.the.game.map.InitialPlayerPosition;
 public class TeleportPlayerSystem extends EntitySystem {
 
     private Dungeon dungeon;
-    private Engines engines;
     private Player player;
 
-    public TeleportPlayerSystem(Dungeon dungeon, Engines engines, Player player) {
+    public TeleportPlayerSystem(Dungeon dungeon, Player player) {
         super(10000);
-
         this.dungeon = dungeon;
-        this.engines = engines;
         this.player = player;
     }
 
