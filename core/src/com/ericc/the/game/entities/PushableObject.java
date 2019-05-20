39f1095a8c7971b108eebc26aca4a83e17591fa3
fiffeek <fm394471@students.mimuw.edu.pl>
2019-05-20 17:08:14 +0200

    Add drop from boxes

--- core/src/com/ericc/the/game/entities/PushableObject.java
+++ core/src/com/ericc/the/game/entities/PushableObject.java
@@ -2,10 +2,8 @@ package com.ericc.the.game.entities;
 
 import com.badlogic.ashley.core.Entity;
 import com.ericc.the.game.Models;
-import com.ericc.the.game.components.AnimationComponent;
-import com.ericc.the.game.components.CollisionComponent;
-import com.ericc.the.game.components.PositionComponent;
-import com.ericc.the.game.components.RenderableComponent;
+import com.ericc.the.game.components.*;
+import com.ericc.the.game.drops.ExampleDrop;
 import com.ericc.the.game.map.Map;
 import com.ericc.the.game.utils.GridPoint;
 
@@ -13,8 +11,10 @@ public class PushableObject extends Entity {
     public PushableObject(GridPoint xy, Map map) {
         RenderableComponent renderable = new RenderableComponent(Models.crate, true);
         add(new PositionComponent(xy, map));
+        add(new StatsComponent(10, 0, 0, 20, false));
         add(new CollisionComponent(CollisionComponent.Type.CRATE));
         add(new AnimationComponent());
         add(renderable);
+        add(new DropComponent(new ExampleDrop()));
     }
 }
